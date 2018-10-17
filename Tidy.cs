8062cffb0cb4077c66811f5e270bd15000b13109
Kevin K <44191626+Kretsche93@users.noreply.github.com>
2018-10-17 13:45:07 +0200

    add new scripts

---
+++ Tidy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Meta/Interaction/TidyScript")]
+public class Tidy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 5f);
+	}
+}
